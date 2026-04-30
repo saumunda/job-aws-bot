f68cbb19b74ce5ce2585e29f5bdf9716b7ac8631
SauMunda <saurabhbhola80@gmail.com>
2026-04-30 14:18:46 +0000

    30/04/2026 latest running

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cd /home/ubuntu/job-aws-bot
+
+echo "Pulling latest code..."
+git pull origin main
+
+echo "Installing dependencies..."
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "Restarting service..."
+sudo systemctl restart job-aws-bot
+
+echo "Done."
