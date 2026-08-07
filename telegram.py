bcfc219b9ea06e65a0bad15d0b5fc51b8a1a5def
Saurabh Bhola <83873339+saumunda@users.noreply.github.com>
2026-08-07 07:53:29 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -7,7 +7,8 @@ def send(msg):
             requests.post(
                 f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
                 data={"chat_id": chat_id, "text": msg, "parse_mode": "Markdown"},
-                timeout=10
+                timeout=30
             )
         except Exception as e:
             print("Telegram error:", e)
+            return "Telegram error:", e"
